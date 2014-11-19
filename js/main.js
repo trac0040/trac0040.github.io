7ffa61cc61535993a11b751e31ca2a68bcd357bf
Marta <trac0040@algonquinlive.com>
2014-11-19 14:04:20 -0500

    Added a back to top header button
    (did not succeed in making a fixed responsive shrinking header)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$('.fig').on('click', function () {
+$(document).ready(function() {
+    $('.fig').on('click', function () {
   var $details = $(this).children('.figcap');
 
   if ($details.attr('data-state') == 'active' ) {
@@ -6,4 +7,18 @@ $('.fig').on('click', function () {
   } else {
     $details.attr('data-state', 'active');
   }
+});
+
+$(window).scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('.scroll-to-top').fadeIn();
+		} else {
+			$('.scroll-to-top').fadeOut();
+		}
+	});
+	
+	$('.scroll-to-top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 });
